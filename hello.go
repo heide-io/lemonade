cdcad9ad670568b5db2ccb292c3c312bd8036cfd
Krystyn Heide <hello+github@krystyn.me>
2016-11-15 15:57:26 -0500

    Testing
    func main: Listening onn

--- hello.go
+++ hello.go
@@ -30,7 +30,7 @@ func getAddr() string {
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("assets")))
 	addr := getAddr()
-	log.Printf("Listening on %s", addr)
+	log.Printf("Listening onn %s", addr)
 	if err := http.ListenAndServe(addr, withLogs(http.DefaultServeMux)); err != nil {
 		log.Fatal(err)
 	}
